8c56afea0c31f2c861ac6d5b6f23cd6559e29571
lulu <10356014@ntub.edu.tw>
2018-05-27 13:58:04 +0800

    test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { KeyboardPage } from '../keyboard/keyboard';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+<<<<<<< HEAD
+  name:any; 
+  city:any; 
+=======
+  name:any;
+  city:any;
 
-  constructor(public navCtrl: NavController) {
+>>>>>>> master
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
   }
-
+ 
+  doConfirm() {
+<<<<<<< HEAD
+    let confirm = this.alertCtrl.create({
+      title: '提示',
+      message: '所選店鋪為「' + this.city + this.name + '」?', 
+      buttons: [
+        {
+          text: '返回',
+          handler: () => {
+            console.log('Disagree clicked');
+=======
+    if (this.city == undefined || this.name == undefined) {
+      let confirm = this.alertCtrl.create({
+        title: '提示',
+        message: '請選擇店鋪縣市及名稱',
+        buttons: [
+          {
+            text: '返回',
+            handler: () => {
+              console.log('Disagree clicked');
+            }
+>>>>>>> master
+          }
+        ] 
+      });
+      confirm.present()
+    }else {
+      let confirm = this.alertCtrl.create({
+        title: '提示',
+        message: '所選店鋪為「' + this.city + this.name + '」',
+        buttons: [
+          {
+            text: '返回',
+            handler: () => {
+              console.log('Disagree clicked');
+            }
+          },
+          {
+            text: '確認',
+            handler: () => {
+              console.log('Agree clicked');
+              this.navCtrl.push(KeyboardPage);
+            }
+          }
+        ]
+      });
+      confirm.present()
+    }
+  }
 }
